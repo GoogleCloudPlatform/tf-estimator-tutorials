5f64946b73733f9f74a5212d22fd9bfb2a8d2818
Lukman Ramsey <lramsey@google.com>
2018-09-10 16:47:03 -0400

    passing eval_data into inference test

--- 00_Miscellaneous/optimize_graph.py
+++ 00_Miscellaneous/optimize_graph.py
@@ -343,7 +343,7 @@ def convert_graph_def_to_saved_model(export_dir, graph_filepath):
 def setup_model():
   train_data, train_labels, eval_data, eval_labels = load_mnist_data()
   export_dir = train_and_export_model(train_data, train_labels)
-  return export_dir
+  return export_dir, eval_data
 
 
 #    'quantize_weights',
@@ -370,11 +370,11 @@ def freeze_model(saved_model_dir):
 
 def main():
 
-  export_dir = setup_model()
+  export_dir, eval_data = setup_model()
   saved_model_dir = os.path.join(export_dir, os.listdir(export_dir)[-1])
   describe_graph(get_graph_def_from_saved_model(saved_model_dir), show_nodes=True)
 
-  inference_test(saved_model_dir=saved_model_dir, signature='serving_default', repeat=10000)
+  inference_test(saved_model_dir=saved_model_dir, eval_data, signature='serving_default', repeat=10000)
 
   frozen_filepath = freeze_model(saved_model_dir)
   describe_graph(get_graph_def_from_file(frozen_filepath), show_nodes=True)
@@ -386,4 +386,4 @@ def main():
 
   convert_graph_def_to_saved_model(optimized_export_dir, optimized_filepath)
 
-  inference_test(saved_model_dir=freezed_saved_model_dir, signature='serving_default', repeat=10000)
+  inference_test(saved_model_dir=freezed_saved_model_dir, eval_data, signature='serving_default', repeat=10000)
